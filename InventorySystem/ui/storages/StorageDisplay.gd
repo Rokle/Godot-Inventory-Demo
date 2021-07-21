8c5bc300b6e608810d237b7b4610825122075015
Rokle <47774579+Rokle@users.noreply.github.com>
2021-07-21 19:44:44 +0300

    Rewrite StorageDisplay.gd
    Same functional but much cleaner code

--- InventorySystem/ui/storages/StorageDisplay.gd
+++ InventorySystem/ui/storages/StorageDisplay.gd
@@ -11,35 +11,29 @@ func _ready():
 	inventory_display = get_parent().find_node("InventorySlotsDisplay")
 
 func _set_storage(storage):
-	if current_storage == null and storage.can_open == true:
+	
+	if storage.can_open == false:
+		_delete_storage()
+		return
+	
+	if storage != current_storage:
+		_delete_storage()
 		current_storage = storage
-		if get_child_count() > 0:
-			_open_storage(false)
-			get_child(0).queue_free()
+		
 		var storage_display = load("res://ui/storages/%s/%s.tscn" % [current_storage.storage_type,current_storage.ui_type]).instance()
 		storage_display.inventory_id = current_storage.inventory_id
 		call_deferred("add_child",storage_display)
-	elif current_storage == storage and storage.can_open == false:
-		if get_child_count() > 0:
-			_open_storage(false)
-			get_child(0).queue_free()
-		current_storage = null
-	elif current_storage != storage and storage.can_open == true:
-		current_storage.opened = false
-		if get_child_count() > 0:
-			_open_storage(false)
-			get_child(0).queue_free()
-		current_storage = storage
-		var storage_display = load("res://ui/storages/%s/%s.tscn" % [current_storage.storage_type,current_storage.ui_type]).instance()
-		storage_display.inventory_id = current_storage.inventory_id
-		call_deferred("add_child",storage_display)
-	elif storage == current_storage:
-		if get_child_count() > 0:
-			_open_storage(current_storage.opened)
-	else:
-		if get_child_count() > 0:
-			_open_storage(false)
-			get_child(0).queue_free()
+		return
+	
+	if storage == current_storage:
+		_open_storage(current_storage.opened)
+		return
+
+
+func _delete_storage():
+	if get_child_count() > 0:
+		_open_storage(false)
+		get_child(0).queue_free()
 		current_storage = null
 
 func _open_storage(state):
