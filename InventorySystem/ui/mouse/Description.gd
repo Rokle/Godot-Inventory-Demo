17fd68a8cddc6f2572290a4582fa9900eb2cae6c
Rokle <47774579+Rokle@users.noreply.github.com>
2022-01-06 20:59:38 +0300

    Update Description.gd

--- InventorySystem/ui/mouse/Description.gd
+++ InventorySystem/ui/mouse/Description.gd
@@ -2,10 +2,12 @@ extends Container
 
 var description
 var background
+var description_container
 
 func _ready():
 	description = $Description
 	background = $NinePatchRect
+	description_container = get_parent()
 	set_process(false)
 
 func _process(_delta):
@@ -14,6 +16,13 @@ func _process(_delta):
 	background.rect_size.x += 8
 	background.rect_size.y += 3
 	set_process(false)
+
+func update_position(mouse_pos):
+	if not description_container.visible:
+		return
+	
+	description_container.position.x = GlobalSettings.window_widht - mouse_pos.x - 6 - background.rect_size.x  if mouse_pos.x + background.rect_size.x + 6 > GlobalSettings.window_widht else 0
+	description_container.position.y = GlobalSettings.window_height - mouse_pos.y - 6 - background.rect_size.y  if mouse_pos.y + background.rect_size.y + 6 > GlobalSettings.window_height else 0
 
 func show_description(id):
 	background.visible = true
