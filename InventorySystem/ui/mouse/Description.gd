94767db722812f0cb58cb21c45e51ed295cf842f
Rokle <47774579+Rokle@users.noreply.github.com>
2022-01-10 23:58:44 +0300

    Update Description.gd

--- InventorySystem/ui/mouse/Description.gd
+++ InventorySystem/ui/mouse/Description.gd
@@ -4,6 +4,9 @@ var description
 var background
 var description_container
 
+var window_height = ProjectSettings.get_setting("display/window/size/height")
+var window_widht = ProjectSettings.get_setting("display/window/size/width")
+
 func _ready():
 	description = $Description
 	background = $NinePatchRect
@@ -21,8 +24,8 @@ func update_position(mouse_pos):
 	if not description_container.visible:
 		return
 	
-	description_container.position.x = GlobalSettings.window_widht - mouse_pos.x - 6 - background.rect_size.x  if mouse_pos.x + background.rect_size.x + 6 > GlobalSettings.window_widht else 0
-	description_container.position.y = GlobalSettings.window_height - mouse_pos.y - 6 - background.rect_size.y  if mouse_pos.y + background.rect_size.y + 6 > GlobalSettings.window_height else 0
+	description_container.position.x = window_widht - mouse_pos.x - 6 - background.rect_size.x  if mouse_pos.x + background.rect_size.x + 6 > window_widht else 0
+	description_container.position.y = window_height - mouse_pos.y - 6 - background.rect_size.y  if mouse_pos.y + background.rect_size.y + 6 > window_height else 0
 
 func show_description(id):
 	background.visible = true
