24bd2b02f1e90888d86a9120c05a55686de44b68
Rokle <47774579+Rokle@users.noreply.github.com>
2022-06-12 00:30:23 +0300

    Delete InventorySystem directory

--- InventorySystem/data/ItemData.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-var item_data
-
-func _ready():
-	var item_data_file = File.new()
-	item_data_file.open("res://data/Items.json",File.READ)
-	item_data = JSON.parse(item_data_file.get_as_text()).result
-	item_data_file.close()
